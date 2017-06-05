1cc31a2907354782df7c517a7753bf0b74fe1ad3
hello <hello@192.168.0.11>
2017-06-05 20:14:43 +0530

    first change

---
+++ src/com/app/servlet/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.app.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@SuppressWarnings("serial")
+@WebServlet("/firstServlet")
+public class LoginServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doPost(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/plain");
+		PrintWriter out=response.getWriter();
+		
+		out.println("<h1>Welcome to Myservlet Page<h1/>");
+		
+		out.close();
+		
+	}
+
+}
